f3f14e822b0485ce3d6903ccf2c9199a98bb9b9f
evotox <dev.mubeenkhan@gmail.com>
2016-02-02 19:09:46 +0500

    Action and Version Change

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*
 	Plugin Name: Evotox Plugin
 	Description: This is for updating your Wordpress plugin.
-	Version: 1.0.1
+	Version: 1.0.2
 	Author: Mubeen Khan
 	Author URI: http://www.evotox.com
 */
@@ -14,3 +14,9 @@ $updater->set_username( 'evotox' );
 $updater->set_repository( 'evotox-plugin-updater' );
 // $updater->authorize( 'abcdefghijk1234567890' ); // Your auth code goes here for private repos
 $updater->initialize();
+
+add_action('wp_footer', 'evotox_test');
+
+function evotox_test(){
+    echo '<h1 style="position: fixed; bottom: 10px; right: 10px;">Hello World</h1>';
+}
